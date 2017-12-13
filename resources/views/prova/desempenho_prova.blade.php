b8779a7935e829b3a690445362932cb7f5fc5b17
Stephanie Benites <benitessl2309@gmail.com>
2017-12-13 17:47:59 -0200

    Incluido botão voltar SBL

--- resources/views/prova/desempenho_prova.blade.php
+++ resources/views/prova/desempenho_prova.blade.php
@@ -39,9 +39,15 @@
 			  </div>
 			</div>
 			</div>
-			@ENDFOREACH
-		  
+			@ENDFOREACH		  
+			<div class="row">
+				<br>
+				<div class="col sm12 m3 right">
+					<button class="waves-effect right waves-light btn orange lighten-1" href="gerenciador_turma.html">Voltar</button>
+				</div>
+			</div>
 	</div>
+
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 	<script src='js/configuracoes-datepicker.js'></script>
