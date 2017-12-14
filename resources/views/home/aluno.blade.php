bb442cf5789f770c9ac4da5f479d1da96b949ac5
Paulo Passos <paulo_santiago@live.com>
2017-12-14 15:37:45 -0200

    Dashboard de aluno Paulo.

--- resources/views/home/aluno.blade.php
+++ resources/views/home/aluno.blade.php
@@ -1,23 +1,60 @@
-@extends('layouts.app')
+@extends('layouts.padrao')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading"> <strong> ALUNO </strong>Dashboard</div>
+@section('titulo','Dashboard Aluno')
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+@section('plugins')
+<link rel="stylesheet" href="{{asset('/fullcalendar/fullcalendar.min.css')}}" />
+<link rel="stylesheet" href="{{asset('/fullcalendar/fullcalendar.print.min.css')}}" />
+<link rel="stylesheet" href="{{asset('/fullcalendar/fullcalendar.print.css')}}" />
+<link rel="stylesheet" href="{{asset('/fullcalendar/fullcalendar.css')}}" />
+<link rel="stylesheet" href="{{asset('/css/dashboard-style.css')}}" />
+@endsection
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@section('conteudo')
+<div class="main">
+		<div class="container-fluid">
+			
+			<div class="row">
+									<div class="col s12 m6">
+										<div class="card deep-orange lighten-1 white-text">  
+											<div class="card-content valign-wrapper">
+												<div class="card-text">
+												<h6>Realizar Prova</h6>
+												<p>Realize a prova liberada para você.</p>
+											</div>
+											<div class="card-icon">
+												<i class="material-icons medium valign">ondemand_video</i>
+											</div>
+											</div>
+											<div class="card-action">
+											<a href="#"><i class="material-icons right">forward</i></a>
+											</div>
+										</div>
+									</div>
+									<div class="col s12 m6">
+										<div class="card light-green white-text">  
+											<div class="card-content valign-wrapper">
+											<div class="card-text">
+												<h6>Consultar Provas</h6>
+												<p>Veja as provas que já foram realizadas.</p>
+											</div>
+											<div class="card-icon">
+												<i class="material-icons medium valign">dvr</i>
+											</div>
+											</div>
+											<div class="card-action">
+											<a href="#"><i class="material-icons right">forward</i></a>
+											</div>
+										</div>
+									</div>
+					</div>
+
+
+	  </div>
+	</div>
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+    <script src="{{ asset('fullcalendar/lib/moment.min.js') }}"></script>
+    <script src="{{ asset('fullcalendar/fullcalendar.js') }}"></script>
+    <script src="{{ asset('fullcalendar/locale/pt-br.js') }}"></script>
 @endsection
