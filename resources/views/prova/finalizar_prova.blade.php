60ebea7355fffd731598a0270b87d6344fa6730c
Paulo Passos <paulo_santiago@live.com>
2017-12-16 02:00:25 -0200

    Finalizar prova
    Signed-off-by: Paulo Passos <paulo_santiago@live.com>

---
+++ resources/views/prova/finalizar_prova.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.padrao')
+
+@section('titulo','Finalizar Prova')
+
+@section('plugins')
+  <link rel="stylesheet" href="{{asset('/fullcalendar/fullcalendar.min.css')}}" />
+	<link rel="stylesheet" href="{{asset('/fullcalendar/fullcalendar.print.min.css')}}" />
+	<link rel="stylesheet" href="{{asset('/fullcalendar/fullcalendar.print.css')}}" />
+	<link rel="stylesheet" href="{{asset('/fullcalendar/fullcalendar.css')}}" />
+	<link rel="stylesheet" href="{{asset('/css/dashboard-style.css')}}" />
+@endsection
+
+@section('conteudo')
+
+@endsection
