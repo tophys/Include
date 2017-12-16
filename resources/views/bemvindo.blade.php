038788ba5f063fa6017761f3db76ae2334c5f7b7
Stephanie Benites <benitessl2309@gmail.com>
2017-12-16 05:02:42 -0200

    wwwwweeeeerrrrr

--- resources/views/bemvindo.blade.php
+++ resources/views/bemvindo.blade.php
@@ -43,7 +43,7 @@
             <span class="cd-words-wrapper waiting">
                 <b class="is-visible">:</b>
                 <b class="deep-orange-text text-lighten-1">educar</b>
-                <b class="blue-text">inovar</b>
+                <b class="white-text">inovar</b>
                 <b class="orange-text text-lighten-1">aprender</b>
             </span>
         </h1>
