c73a2d78f6cfd0f55a0f9ab56d104067a3d8c12e
Paulo Passos <paulo_santiago@live.com>
2017-12-16 03:07:52 -0200

    Finalizar prova, beta
    Signed-off-by: Paulo Passos <paulo_santiago@live.com>

--- resources/views/prova/finalizar_prova.blade.php
+++ resources/views/prova/finalizar_prova.blade.php
@@ -11,5 +11,29 @@
 @endsection
 
 @section('conteudo')
+<div class="main">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col s12 m4">
+						<img src="{{asset('/images/img-final.png')}}">
+            </div>
+						<div class="col sm12 m4 center">
+						<a class="waves-effect right waves-light btn orange lighten-1" href="{{url('/dashboard/aluno')}}">Voltar para a Home</a>
+						</div>    
+        </div>
+    </div>
+</div>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+<script src="{{asset('js/configuracoes-datepicker.js')}}"></script>
+<script>
+    $(document).ready(function() {
 
+        // page is now ready, initialize the calendar...
+
+        $('#calendar').fullCalendar({
+            // put your options and callbacks here
+        })
+    });
+</script>
 @endsection
