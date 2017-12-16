be3a2fc86363ac1260f73f8570f97bd24c3d5663
Stephanie Benites <benitessl2309@gmail.com>
2017-12-16 03:59:43 -0200

    css da prova

--- resources/views/perfil/perfil.blade.php
+++ resources/views/perfil/perfil.blade.php
@@ -1,27 +1,34 @@
 @extends('layouts.padrao')
 
 @section('titulo','Perfil {{Auth::user()->name}}')
+<link rel="stylesheet" href="{{asset('/css/dashboard-style.css')}}" />
 
 @section('conteudo')
 <div class="main">
 	  <div class="container-fluid">
 		<div class="row">
-            <div class="col-md-10 col-md-offset-1">
+                <form enctype="multipart/form-data" action="/perfil" method="post">
+            <div class="col-md-6 col-md-offset-3">
                 <img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left;  border-radius: 50%;" />
                 <h2>{{ $user->name }}</h2>
-                <form enctype="multipart/form-data" action="/perfil" method="post">
-                <label><b>Selecione uma imagem de perfil: </b></label></br>
-                <input type="file" name="avatar" /></br>
-                <input type="submit" />
+                <div class="file-field input-field">
+										<div class="btn light-green">
+											<span>Selecionar</span>
+											<input type="file"  name="avatar" />
+										</div>
+										<div class="file-path-wrapper">
+											<input class="file-path validate" type="text" placeholder="Selecione sua imagem de perfil">
+										</div>
+									</div>
+            </div>
+            <div class="col-md-6 col-md-offset-3">
+                  <button class="waves-effect center waves-light btn orange lighten-1">Enviar foto</button>
+            </div>
                 {{ csrf_field() }}
                 </form>
-            </div>
 		</div>
 	  </div>
 	</div>
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-  <script src="{{ asset('fullcalendar/lib/moment.min.js') }}"></script>
-	<script src="{{ asset('fullcalendar/fullcalendar.js') }}"></script>
-	<script src="{{ asset('fullcalendar/locale/pt-br.js') }}"></script>
 @endsection
